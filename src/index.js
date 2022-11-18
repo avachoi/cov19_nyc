8eedf1b8e5adc8ac679a027c476a47e87bcc4ea4
avachoi <alexbchoi@gmail.com>
2022-11-18 17:59:46 -0500

    feat: set-up local host

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+import { render } from "react-dom";
+
+const App = () => {
+	return <div>'Ava'</div>;
+};
+
+render(<App />, document.getElementById("app"));
