76352a574c14d46771896b30dc193364d7bdde9a
avachoi <alexbchoi@gmail.com>
2022-11-18 19:58:31 -0500

    feat: pagination

--- src/index.js
+++ src/index.js
@@ -1,23 +1,72 @@
 import React, { useEffect } from "react";
 import { render } from "react-dom";
 import Header from "./components/header.js";
+import CaseContainer from "./components/caseContainer.js";
+import Pagination from "./components/pagination.js";
 import Footer from "./components/footer.js";
+const axios = require("axios");
 
 const App = () => {
+	const [covidData, setCovidData] = React.useState([]);
+	const [sort, setSort] = React.useState("zip");
+	const [showPerPage, setSortPerPage] = React.useState(20);
+	const [currentPage, setCurrentPage] = React.useState(1);
 	const [currentTab, setCurrentTab] = React.useState("table-tab");
+	const [zipSearch, setZipSearch] = React.useState("");
+
+	useEffect(() => {
+		axios.get("api/covid").then((res) => {
+			setCovidData(res.data);
+		});
+	}, []);
+
+	function handleChange(sortBy) {
+		setSort(sortBy);
+		setCurrentPage(1);
+	}
+
+	function handlePagination(page) {
+		setCurrentPage(page);
+	}
 
 	function handleToggle(tab) {
 		setCurrentTab(tab);
 	}
 
+	let sorted, beforeFilter, data;
+
+	function handleToggle(tab) {
+		setCurrentTab(tab);
+	}
+
+	//sort whether data will be shown by zip or positive cases
+	if (sort === "zip") {
+		sorted = covidData;
+	} else {
+		sorted = covidData.slice().sort((a, b) => {
+			return b.positive - a.positive;
+		});
+	}
+
 	//determine which component to show depending on which tab is clicked
 	let main;
 	if (currentTab === "table-tab") {
-		let main = (
+		main = (
 			<main>
 				<div className="container">
-					<CaseContainer />
+					<CaseContainer
+						cases={data}
+						showPerPage={showPerPage}
+						currentPage={currentPage}
+					/>
+					<Pagination
+						onPageChange={handlePagination}
+						showPerPage={showPerPage}
+						dataLength={beforeFilter.length}
+						currentPage={currentPage}
+					/>
 				</div>
+				<FilterForm onDropDownChange={handleChange} sort={sort} />
 			</main>
 		);
 	} else {
